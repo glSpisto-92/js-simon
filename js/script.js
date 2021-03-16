3201b6c9654c46eb703f60bd0c7db4cf94e5972f
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-03-16 15:16:47 +0100

    Aggiunto progetto js simon (da sistemare)

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(function(){
+  // Un alert() espone 5 numeri generati casualmente.
+  // Da li parte un timer di 30 secondi.
+  // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente,
+   // tramite il prompt().
+  // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri
+   // da indovinare sono stati individuati.
+  //
+
+  // funzione che genera numeri random
+  function numRandom(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  var numeriRandom;
+
+  // array dove verranno salvati i numeri dell'alert
+  var numeriUtente = [];
+  numeriInput = [];
+
+  // ciclo finchè i numeri random arrivano a 5 e li  pusho in un array
+  while (numeriUtente.length < 5){
+
+    numeriRandom = numRandom(1, 100);
+    numeriUtente.push(numeriRandom);
+
+  }
+  // mostro a schermo i 5 numeri random
+  alert(numeriUtente);
+
+  var  inputUtente;
+
+// setto una funzione di timing dopo aver inserito i numeri
+  setTimeout(function () {
+
+    // creo un ciclo per far inserire all'utente 5 numeri
+    while (numeriInput.length < 5){
+
+      inputUtente = parseInt(prompt('Inserisci 1 alla volta i 5 numeri'));
+      numeriInput.push(inputUtente);
+      // if( numeriInput.includes(numeriRandom.length)){
+      //   console.log(numeriInput);
+      // }
+    }
+    if(numeriInput.includes(numeriRandom)) {
+    console.log('Bravo! hai ricordato ' + numeriInput.length + ' numeri : ' + numeriInput);
+    }
+  }, 2000);
+
+});
